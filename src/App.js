1d1459d43379f62750459aaa1500236898dbba06
Arseid <yvesrobert.estrada@gmail.com>
2023-02-27 14:44:07 +0100

    Switch layouts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Layout from "./layout";
 
 function App() {
   return (
     <div className="App" style={{height: "100 vh", width: "100 vw"}}>
-      Yo
+      <Layout/>
     </div>
   );
 }
--- src/layout.jsx
+++ src/layout.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Gacha from "./pages/gacha/gacha";
+import Home from "./pages/home/home";
+import Minigames from "./pages/minigames/minigames";
 
 const Layout = () => {
+    const layoutList = [Gacha,Home,Minigames];
+    const [currentLayout, setCurrentLayout] = React.useState(layoutList[0]);
+
+    const changeLayout = (id) => setCurrentLayout(layoutList[id]);
+
     return <div>
+        {currentLayout}
+        <button onClick={()=>{changeLayout(0)}}>Gacha</button>
+        <button onClick={()=>{changeLayout(1)}}>Home</button>
+        <button onClick={()=>{changeLayout(2)}}>Minigames</button>
     </div>
 };
 
