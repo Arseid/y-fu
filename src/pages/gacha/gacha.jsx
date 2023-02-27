b07998e968cf00bebfbc6d1e3d5d825a80b70d4e
Arseid <yvesrobert.estrada@gmail.com>
2023-02-27 14:21:43 +0100

    Base Project

---
+++ src/pages/gacha/gacha.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Gacha = () => {
+    return <div>
+
+    </div>
+};
+
+export default Gacha;
---
+++ src/pages/minigames/minigames.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Minigames = () => {
+    return <div>
+
+    </div>
+};
+
+export default Minigames;
