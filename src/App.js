a1807f7998c35952109e80a8af94ddae6ba875c4
Arseid <yvesrobert.estrada@gmail.com>
2023-02-27 16:54:29 +0100

    Switch layouts with background color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import Layout from "./layout";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App" style={{height: "100 vh", width: "100 vw"}}>
-      <Layout/>
-    </div>
-  );
+    const layoutList = ["Gacha","Home","Minigames"];
+    const [currentLayout, setCurrentLayout] = React.useState(layoutList[0]);
+    const changeLayout = (id) => setCurrentLayout(layoutList[id]);
+
+    return (
+        <div className="App" style={{
+            backgroundColor: currentLayout === 'Gacha' ? 'red' : currentLayout === 'Home' ? 'orange' : 'purple',
+            position:'absolute',
+            left:0,
+            top:0,
+            width:'100%',
+            height:'100%'
+        }}>
+            <Layout currentLayout={currentLayout} changeLayout={changeLayout}/>
+        </div>
+    );
 }
 
 export default App;
--- src/layout.jsx
+++ src/layout.jsx
@@ -3,17 +3,25 @@ import Gacha from "./pages/gacha/gacha";
 import Home from "./pages/home/home";
 import Minigames from "./pages/minigames/minigames";
 
-const Layout = () => {
-    const layoutList = [Gacha,Home,Minigames];
-    const [currentLayout, setCurrentLayout] = React.useState(layoutList[0]);
+const Layout = ({changeLayout, currentLayout}) => {
 
-    const changeLayout = (id) => setCurrentLayout(layoutList[id]);
+    const layout = (currentLayout) => {
+        let tempLayout;
+        if (currentLayout === 'Gacha') tempLayout = (<Gacha/>)
+        else if (currentLayout === 'Home') tempLayout = (<Home/>)
+        else if (currentLayout === 'Minigames') tempLayout = (<Minigames/>)
+        return tempLayout;
+    }
 
     return <div>
-        {currentLayout}
-        <button onClick={()=>{changeLayout(0)}}>Gacha</button>
-        <button onClick={()=>{changeLayout(1)}}>Home</button>
-        <button onClick={()=>{changeLayout(2)}}>Minigames</button>
+        {layout(currentLayout)}
+        <div style={{bottom: 0, position: "absolute"}}>
+            <div style={{display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                <button onClick={()=>{changeLayout(0)}}>Gacha</button>
+                <button onClick={()=>{changeLayout(1)}}>Home</button>
+                <button onClick={()=>{changeLayout(2)}}>Minigames</button>
+            </div>
+        </div>
     </div>
 };
 
